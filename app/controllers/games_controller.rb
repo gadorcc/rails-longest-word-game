55cb4728c5cb7694f9ecd9030f430a9820085c5b
Gador Cruz <gadorcruz@hotmail.com>
2023-01-20 17:41:45 +0000

    some styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,20 +1,27 @@
 class GamesController < ApplicationController
   require 'open-uri'
   require "json"
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = ('a'..'z').to_a.shuffle[0..9].join(' ').upcase
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
   def score
-    @word = params[:word]
+    @word = params[:word].upcase
+    @wordarray = @word.split('').to_a
+    @letters = params[:letters]
     @url= "https://wagon-dictionary.herokuapp.com/#{@word}"
     @dictionary = URI.open(@url).read
     api = JSON.parse(@dictionary)
-    if(api["found"] == true)
+    if((@wordarray.all? {|e| @letters.include?(e)}) == false)
+      p @answer = "Sorry but #{@word} can't be build with #{@letters} "
+    elsif(api["found"] == true)
       p @answer = "Congratulations! #{@word} is a valid English word!"
     else
-      p@answer = "Sorry but #{@word} is not a English word"
+      p @answer = "Sorry but #{@word} is not a English word"
     end
 
-    # puts @test = api["found"]
   end
 end
